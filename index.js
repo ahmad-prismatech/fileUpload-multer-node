464ee448bfce506aceddf81eb597b82ef79a716a
Ahmad Raza <raza88581@gmail.com>
2021-11-08 16:59:41 +0500

    changing port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const fileRoutes = require("./routes/file-upload-routes");
 const 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 
